134808648cbf88ecf66e2db300043c2ba38f9f7a
❌_N_I_K_K_A_❌ <87988890+NIKKAvRULZ@users.noreply.github.com>
2024-09-07 00:04:55 +0530

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const dbConeection = require("./config/db");
 const routes = require("./routes/employees");
+const routes = require("./routes/cuts");
+const routes = require("./routes/assign");
+const routes = require("./routes/notifications");
+const routes = require("./routes/status");
+const routes = require("./routes/track");
+const routes = require("./routes/home");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -16,7 +22,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",(req,res) => res.send("Hello World"));
 
 app.use("/api/employees",routes);
- 
+app.use("/api/cuts",routes);
+app.use("/api/assign",routes);
+app.use("/api/assign",routes);
+app.use("/api/notifications",routes);
+app.use("/api/status",routes);
+app.use("/api/track",routes);
+app.use("/api/home",routes);
+
 const PORT = 3000;
 
 app.listen(PORT,() => console.log(`Server Running On PORT ${PORT}`)); 
